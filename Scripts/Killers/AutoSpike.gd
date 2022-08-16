5cc63403afaaa2a8c5c725405e0a99eb9db4784a
Smartkin <vladislav5019@gmail.com>
2022-08-17 00:01:49 +0300

    Update the AutoSpike

--- Scripts/Killers/AutoSpike.gd
+++ Scripts/Killers/AutoSpike.gd
@@ -1,34 +1,52 @@
+tool
 extends KinematicBody2D
 
 onready var n_SpikeOrientation := $SpikeOrientation
 onready var n_BlockChecker := $BlockChecker
 
-export(int, -1, 3) var spike_dir := -1
+enum SPIKE_DIRECTION {
+	AUTO = -1,
+	RIGHT,
+	DOWN,
+	LEFT,
+	UP,
+}
+
+export(SPIKE_DIRECTION) var spike_dir := SPIKE_DIRECTION.AUTO
 var check_dir := 0
 
 
 func _ready() -> void:
 	add_to_group("Killers")
-	if spike_dir != -1:
+	if spike_dir != SPIKE_DIRECTION.AUTO:
 		set_spike_dir(spike_dir)
 	else:
 		_check_for_solids()
+
+func _process(delta):
+	if Engine.editor_hint:
+		if spike_dir != SPIKE_DIRECTION.AUTO:
+			set_spike_dir(spike_dir)
+		else:
+			_check_for_solids()
 
 
 func set_spike_dir(value: int) -> void:
 	spike_dir = value
 	match(value):
-		0: 
+		SPIKE_DIRECTION.RIGHT: 
 			n_SpikeOrientation.play("right")
-		1:
+		SPIKE_DIRECTION.DOWN:
 			n_SpikeOrientation.play("down")
-		2:
+		SPIKE_DIRECTION.LEFT:
 			n_SpikeOrientation.play("left")
+		SPIKE_DIRECTION.UP:
+			n_SpikeOrientation.play("RESET")
 
 
 func _check_for_solids() -> void:
 	for i in range(4):
-		if spike_dir != -1:
+		if spike_dir != SPIKE_DIRECTION.AUTO:
 			break
 		check_dir = i
 		var dir = deg2rad(90 * i)
