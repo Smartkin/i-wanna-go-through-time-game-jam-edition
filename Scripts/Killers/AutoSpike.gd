4c5bc8b66055177ba3b47eb7ef30dfd41b7e40a0
Tralexium <alextheloler@gmail.com>
2022-08-16 22:54:02 +0100

    Fixed autospikes not listening to the exported var

--- Scripts/Killers/AutoSpike.gd
+++ Scripts/Killers/AutoSpike.gd
@@ -23,7 +23,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if spike_dir != SPIKE_DIRECTION.AUTO:
 		set_spike_dir(spike_dir)
-	if check_dir < SPIKE_DIRECTION.LEFT:
+	elif check_dir < SPIKE_DIRECTION.LEFT:
 		_check_for_solids()
 
 
