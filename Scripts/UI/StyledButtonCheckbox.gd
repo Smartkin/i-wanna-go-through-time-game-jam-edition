6f8f5807363550b4f2f359fa0914d92cac002175
Tralexium <alextheloler@gmail.com>
2022-08-17 22:30:43 +0100

    Fixed keybind labels being hidden

--- Scripts/UI/StyledButtonCheckbox.gd
+++ Scripts/UI/StyledButtonCheckbox.gd
@@ -12,14 +12,15 @@ func _ready() -> void:
 	toggle_lbl.name = "toggle_lbl"
 	toggle_lbl.theme = theme
 	toggle_lbl.add_font_override("font", get_font("font"))
+	toggle_lbl.add_constant_override("shadow_offset_y", 0)
 	toggle_lbl.size_flags_horizontal = SIZE_EXPAND_FILL
 	toggle_lbl.size_flags_vertical = SIZE_EXPAND_FILL
 	toggle_lbl.align = toggle_lbl.ALIGN_RIGHT
 	toggle_lbl.valign = toggle_lbl.VALIGN_CENTER
 	toggle_lbl.text = right_lbl
 	prev_right_lbl = right_lbl
-	toggle_lbl.margin_left = margin_left
-	toggle_lbl.margin_right = margin_right
+#	toggle_lbl.margin_left = margin_left
+#	toggle_lbl.margin_right = margin_right
 
 func _process(delta: float) -> void:
 	if (prev_right_lbl != right_lbl):
