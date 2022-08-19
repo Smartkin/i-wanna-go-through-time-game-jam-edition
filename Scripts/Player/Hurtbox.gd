d17eade2fbecd44543e48b6147e7eaea2c9bd268
Smartkin <vladislav5019@gmail.com>
2022-08-19 20:16:07 +0300

    Fix spike precision issue

--- Scripts/Player/Hurtbox.gd
+++ Scripts/Player/Hurtbox.gd
@@ -3,7 +3,13 @@ extends KinematicBody2D
 signal hurt
 
 func _physics_process(delta: float):
-	var collision = move_and_collide(Vector2.ZERO, true, true, true)
-	if (collision != null and collision.collider as Node != null):
-		emit_signal("hurt", collision.collider as Node)
+#	set_sync_to_physics(false)
+	move_and_slide(Vector2.ZERO,  Vector2.UP)
+	position = Vector2.ZERO
+#	set_sync_to_physics(true)
+	var col_cnt = get_slide_count()
+	for i in range(col_cnt):
+		var collision = get_slide_collision(i)
+		if (collision.collider as Node != null):
+			emit_signal("hurt", collision.collider as Node)
 	
