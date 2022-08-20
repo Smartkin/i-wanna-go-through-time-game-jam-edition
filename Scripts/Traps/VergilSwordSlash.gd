563517b02269c9ff4d52380da407cdf5a839f31c
Tralexium <alextheloler@gmail.com>
2022-08-20 23:23:00 +0100

    Almost there with Vergil trap

--- Scripts/Traps/VergilSwordSlash.gd
+++ Scripts/Traps/VergilSwordSlash.gd
@@ -6,14 +6,15 @@ var speed_mult := 1.0
 func _ready() -> void:
 	modulate.a = 0.0
 	rotation_degrees = rand_range(0.0, 360.0)
-	scale.x = 100.0
+	scale.x = 1000.0
 	scale.y = 0.0
 	var tween := create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
 	tween.tween_property(self, "modulate:a", 0.5, 1.0)
-	tween.parallel().tween_property(self, "scale:y", 0.3, 1.0)
+	tween.parallel().tween_property(self, "scale:y", 0.15, 1.0)
 
 
 func slash() -> void:
+	scale.y = 1.5
 	$CollisionShape2D.disabled = false
 	var tween := create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	tween.tween_property(self, "modulate:a", 1.0, 0.1)
@@ -21,6 +22,7 @@ func slash() -> void:
 
 
 func withdraw() -> void:
+	$CollisionShape2D.disabled = true
 	var tween := create_tween().set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
 	tween.tween_property(self, "modulate:a", 0.0, 0.3)
 	tween.parallel().tween_property(self, "scale:y", 0.0, 0.3 * speed_mult)
--- Scripts/Traps/VergilTrap.gd
+++ Scripts/Traps/VergilTrap.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Position2D
 
 export var fx_fade_dur := 2.0
 export var fx_vignette_alpha := 0.7
@@ -10,8 +10,8 @@ export var fx_red := 0.0
 export var fx_green := 0.7
 export var fx_blue := 0.7
 # Glitch effect
-export var fx_shake_pow := 0.01
-export var fx_color_rate := 0.005
+export var fx_shake_pow := 0.05
+export var fx_color_rate := 0.01
 # Anti fisheye
 export var fx_fisheye := -0.15
 
@@ -27,31 +27,33 @@ var color_rate := 0.0
 var fisheye := 0.0
 
 var active := false
+var is_slashing := false
+var has_hit_player := false
 var blades_counts := [3, 3, 3, 8]
 var blade_spawn_delay := 0.3
-var blade_slash_delays := [0.2, 0.2, 0.25]
+var blade_slash_delays := [0.2, 0.2, 0.25, 0.01]
 var target_player: Player = null
-var target_slowdown = 100
-var game_spd = 1
+var target_slowdown := 500.0
+var game_spd := 1.0
 
+onready var n_SwordSlashes := $SwordSlashes
 onready var n_Vignette := $ScreenFX/Vignette
 onready var n_ScreenTint := $ScreenFX/ScreenTint
 onready var n_Glitch := $ScreenFX/Glitch
 onready var n_AntiFisheye := $ScreenFX/AntiFisheye
-onready var n_YouShallDie := $YouShallDie
-onready var n_SwordSlashes := $SwordSlashes
+onready var n_YouShallDie := $YouShallDieSfx
+onready var n_SwordSlashesSfx := $SwordSlashesSfx
+onready var n_KidSlashedSfx := $KidSlashedSfx
+onready var n_VideoPlayer := $VideoLayer/VideoPlayer
 
 var s_SwordSlash := preload("res://Scenes/Traps/VergilSwordSlash.tscn")
-
-
-func _ready() -> void:
-	pass
 
 
 func _process(delta: float) -> void:
 	if active:
 		update_shader_uniforms()
-		Engine.time_scale = game_spd
+		if !has_hit_player or !is_slashing:
+			Engine.time_scale = game_spd
 
 
 func update_shader_uniforms() -> void:
@@ -70,11 +72,15 @@ func update_shader_uniforms() -> void:
 
 
 func _on_Trigger_body_entered(body: Player) -> void:
+	if active:
+		return
+	
 	active = true
 	target_player = body
+	n_SwordSlashes.visible = true
 	enable_fx()
 	spawn_blades()
-	WorldController.fade_music()
+#	WorldController.fade_music()  #TODO: make it only fade the music!
 	n_YouShallDie.play()
 
 
@@ -91,25 +97,85 @@ func enable_fx() -> void:
 	tween.parallel().tween_property(self, "fisheye", fx_fisheye, fx_fade_dur)
 
 
+func disable_fx() -> void:
+	var tween = create_tween().set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, "game_spd", 1.0, (fx_fade_dur/2.0) / target_slowdown)
+	tween.parallel().tween_property(n_Vignette, "self_modulate:a", 0.0, fx_fade_dur/2.0)
+	tween.parallel().tween_property(self, "brightness", 0.0, fx_fade_dur/2.0)
+	tween.parallel().tween_property(self, "contrast", 1.0, fx_fade_dur/2.0)
+	tween.parallel().tween_property(self, "saturation", 1.0, fx_fade_dur/2.0)
+	tween.parallel().tween_property(self, "red", 1.0, fx_fade_dur/2.0)
+	tween.parallel().tween_property(self, "green", 1.0, fx_fade_dur/2.0)
+	tween.parallel().tween_property(self, "blue", 1.0, fx_fade_dur/2.0)
+	tween.parallel().tween_property(self, "fisheye", 0.0, fx_fade_dur/2.0)
+	yield(tween, "finished")
+	# Trap passed, delete to conserve memory
+	queue_free()
+
+
 func spawn_blades() -> void:
 	var rand_offset
 	if target_player != null:
 		for i in blades_counts.size():
-			rand_offset = 20.0
+			rand_offset = 30.0
 			for blade in blades_counts[i]:
 				var slash = s_SwordSlash.instance()
 				var rand_vec = Vector2(rand_range(-rand_offset, rand_offset), rand_range(-rand_offset, rand_offset))
 				slash.global_position = target_player.global_position + rand_vec
-				n_SwordSlashes.add_child(slash)
-				rand_offset += 100.0
+				n_SwordSlashes.call_deferred("add_child", slash)
+				slash.connect("body_entered", self, "_blade_has_touched_player")
+				rand_offset += 150.0
 			# Wait until we can spawn the next wave of blades
 			yield(get_tree().create_timer(blade_spawn_delay), "timeout")
+	slow_down_time()
 
 
 func slow_down_time() -> void:
 	var tween = create_tween().set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_SINE)
-	tween.tween_property(self, "game_spd", 0.0/target_slowdown, 2.0)
+	tween.tween_property(self, "game_spd", 1.0/target_slowdown, 0.5)
 
 
-func _on_YouShallDie_finished() -> void:
-	pass
+func _blade_has_touched_player(body: Node) -> void:
+	has_hit_player = true
+	Engine.time_scale = 1.0
+	$ScreenFX.visible = false
+	$VideoLayer.visible = true
+	n_VideoPlayer.play()
+	n_SwordSlashesSfx.stop()
+	n_KidSlashedSfx.play()
+	if target_player != null:
+		target_player.run_speed = 0
+		target_player.gravity = 0
+
+
+func _on_YouShallDieSfx_finished() -> void:
+	var blade_id := 0
+	
+	is_slashing = true
+	n_SwordSlashesSfx.play()
+	
+	for i in blades_counts.size():
+		if has_hit_player:
+			break
+		
+		shake_pow = fx_shake_pow
+		color_rate = fx_color_rate
+		var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+		tween.tween_property(self, "shake_pow", 0.0, 0.1 / target_slowdown)
+		tween.parallel().tween_property(self, "color_rate", 0.0, 0.1 / target_slowdown)
+			
+		for blade in blades_counts[i]:
+			n_SwordSlashes.get_child(blade_id).speed_mult = game_spd
+			n_SwordSlashes.get_child(blade_id).slash()
+			blade_id += 1
+		# Wait until we can slash more blades
+		yield(get_tree().create_timer(blade_slash_delays[i] / target_slowdown), "timeout")
+
+
+func _on_SwordSlashesSfx_finished() -> void:
+	if has_hit_player:
+		return
+	
+	for blade in n_SwordSlashes.get_children():
+		blade.withdraw()
+	disable_fx()
