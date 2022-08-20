54ff4914c20f22ca12a561cb38b4b8d8e49c6171
Tralexium <alextheloler@gmail.com>
2022-08-20 15:12:32 +0100

    Added breakable blocks

--- Scripts/Bullet/Bullet.gd
+++ Scripts/Bullet/Bullet.gd
@@ -6,14 +6,17 @@ var stats := {
 	damage = WorldController.cur_save_data.gun_power
 }
 
+
 func _ready() -> void:
 	$Timer.start()
 
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	linear_velocity = speed
-	if (state.get_contact_count() != 0 or $EnemyCheck.is_colliding()):
+	if (state.get_contact_count() != 0):
 		if ($EnemyCheck.is_colliding()):
 			$EnemyCheck.get_collider().get_parent().damaged(self)
+		if ($BreakableBlockCheck.is_colliding()):
+			$BreakableBlockCheck.get_collider().break_block()
 		destroy()
 
 func destroy():
