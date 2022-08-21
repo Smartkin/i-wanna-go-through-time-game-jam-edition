62dea8c7cc0863cb30930f6f15aa5a5f2befb6d5
Smartkin <vladislav5019@gmail.com>
2022-08-21 17:46:54 +0300

    Fix bullet ray casting

--- Scripts/Bullet/Bullet.gd
+++ Scripts/Bullet/Bullet.gd
@@ -10,6 +10,11 @@ var stats := {
 func _ready() -> void:
 	$Timer.start()
 
+func _enter_tree():
+	if speed.x < 0:
+		$EnemyCheck.cast_to = Vector2(-$EnemyCheck.cast_to.x, $EnemyCheck.cast_to.y)
+		$BreakableBlockCheck.cast_to = Vector2(-$BreakableBlockCheck.cast_to.x, $BreakableBlockCheck.cast_to.y)
+
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	linear_velocity = speed
 	if (state.get_contact_count() != 0):
