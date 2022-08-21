b17ff70ba8d8363b57086e34ea87052db4351b39
Smartkin <vladislav5019@gmail.com>
2022-08-21 17:20:02 +0300

    Add resave areas to saves

--- Scripts/Intro.gd
+++ Scripts/Intro.gd
@@ -4,7 +4,7 @@ func _ready():
 	$IntroPlayer.play("PlayerWalk")
 	$"%Skip".text = "Press " + InputMap.get_action_list("skip")[0].as_text() + " to skip"
 	var fade_tween = create_tween()
-	fade_tween.tween_property($"%Skip", "self_modulate", Color8(255, 255, 255, 0), 5.0)
+	fade_tween.tween_property($"%Skip", "self_modulate", Color8(255, 255, 255, 0), 3.0)
 
 func _input(event: InputEvent):
 	if (event.is_action_pressed("skip")):
